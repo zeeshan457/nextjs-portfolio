91e38698608f66bb46583d4efab35708665b979d
zeeshan <zeeshanm2405@gmail.com>
2024-04-30 15:02:40 +0100

    animation commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-800">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-secondary-800">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -42,13 +42,14 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-800 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-700 to-secondary-800 hover:bg-slate-200 text-white"
+
             >
-              Hire Me
+              Contact Me
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-800 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-700 to-secondary-800 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
@@ -62,15 +63,17 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/projects/avatar.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={400}
-              height={400}
-            />
-          </div>
+<div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
+  <div className="relative w-full h-full">
+    <Image
+      src="/images/projects/avatar.png"
+      alt="hero image"
+      className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 blur-on-hover"
+      width={400}
+      height={400}
+    />
+  </div>
+</div>
         </motion.div>
       </div>
     </section>
